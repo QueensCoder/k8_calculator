f94898ed4fe3c526a297921ee997d0fdeb0e0767
Ozal Khan <ozalkhan196@gmail.com>
2019-10-24 16:27:45 -0400

    first commit, adding worker and server folders pre container setup

---
+++ worker/index.js
@@ -0,0 +1,13 @@
+const { redisHost: host, redisPort: port } = require('./keys');
+const { redisClient } = require('redis');
+
+// eslint-disable-next-line camelcase
+const redisCli = redisClient({ host, port, retry_strategy: () => 1000 });
+
+const sub = redisCli.duplicate();
+
+const fib = idx => (idx > 2 ? 1 : fib(idx - 1) + fib(idx - 2));
+
+sub.on('message', (channel, message) => {
+  redisCli.hset('values', message, fib(parseInt));
+});
